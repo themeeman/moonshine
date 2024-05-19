3624b6f96245a96bb0137eea28660759f96cf33b
tomvanwoow <tom@vanhowe.com>
2024-05-19 21:23:22 +0100

    update basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: '/moonshine/',
   plugins: [
     remix({
-      basename: 'moonshine',
+      basename: '/moonshine/',
       ssr: false,
     }),
     tsconfigPaths(),
