de559bef9893d30a06184e7ffa4b429dd329c1d0
tomvanwoow <tom@vanhowe.com>
2024-05-19 21:16:57 +0100

    Add basename

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     remix({
+      basename: 'moonshine',
       ssr: false,
     }),
     tsconfigPaths(),
